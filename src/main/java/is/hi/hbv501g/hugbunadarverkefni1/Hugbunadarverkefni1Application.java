a61103564e8d6cae480de47a6b6be7d7b80eca87
Oli <olp10@hi.is>
2023-01-28 13:36:34 +0000

    Fleiri test fyrir railway

--- src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
+++ src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
@@ -13,14 +13,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-/**
- * This is the main class. It uses Spring Boot to launch the application and can be used to
- * modify various settings and properties for it. Here the @EnableJpaRepositories
- * annotation tells JPA to implement repository interfaces automatically.
- *
- * @author Ólafur Pálsson, Brynjólfur Steingrímsson
- */
-
 @SpringBootApplication
 // @EnableJpaRepositories
 public class Hugbunadarverkefni1Application extends SpringBootServletInitializer {
