d72c225b4f1109257fdc2b77101c6f07e7ec869e
BrynjolfurS <brynjolfurstein@gmail.com>
2023-04-19 15:37:15 +0000

    Requestmapping & stuff fyrir clubs

--- src/main/java/is/hi/hbv501g/SportAppBackend/Services/SportService.java
+++ src/main/java/is/hi/hbv501g/SportAppBackend/Services/SportService.java
@@ -2,9 +2,7 @@ package is.hi.hbv501g.SportAppBackend.Services;
 
 import is.hi.hbv501g.SportAppBackend.Persistence.Entities.Club;
 import is.hi.hbv501g.SportAppBackend.Persistence.Entities.Event;
-import is.hi.hbv501g.SportAppBackend.Persistence.Entities.SportModerator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface SportService {
@@ -12,8 +10,9 @@ public interface SportService {
     List<Event> findAllEventsBySport(String sport);
     Club saveClub(Club club);
     public  Boolean isSport(String sport);
-    void  deletClubById(long id);
+    void deleteClubById(long id);
     Event saveEvent(Event event);
     void  deletEventById(long id);
     List<Club> findAllClubsBySport(String sport);
+    Club getClubById(Long id);
 }
